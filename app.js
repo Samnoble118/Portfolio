f9a45f85a12ba2f2c680104331e431f03d36766a
Samnoble118 <samnoble118@icloud.com>
2021-08-05 12:58:13 +0100

    updates to nav bar

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
 tl.to('.into', {y: "-100%", duration: 1}, "-=1");
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+   
+    
+    burger.addEventListener('click', () => {
+        //togggle nav
+        nav.classList.toggle('nav-active');
+
+    });
+
+    //burger animation
+    burger.classList.toggle('toggle');
+    console.log(toggle);
+}
+
+navSlide();
